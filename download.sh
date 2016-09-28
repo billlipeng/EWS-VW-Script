523d1faf3a646fcc0cd4f7d6ea8d014619962d31
Xero <billlipeng@gmail.com>
2016-09-28 13:57:17 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_01.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_03.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_05.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_07.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_09.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_11.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_01.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_03.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_05.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_07.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_09.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_11.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_01.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_03.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_05.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_07.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_09.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_11.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2016_01.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_meta.csv.7z      
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_truth.csv.7z
